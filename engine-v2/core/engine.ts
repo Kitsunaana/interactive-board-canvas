8cfd8f2c5d379ecb4c20031ec55c8fb742d2615a
Kitsunaana <selimkhanov_deni@list.ru>
2026-03-04 20:01:54 +0700

    feature: draw webgl triangle

--- engine-v2/core/engine.ts
+++ engine-v2/core/engine.ts
@@ -21,6 +21,8 @@ export class Application {
   private _renderer: GLRenderer
   private _shader!: Shader
 
+  private _buffer!: WebGLBuffer
+
   public constructor(options: ApplicationOptions) {
     this._renderer = new GLRenderer()
 
@@ -30,9 +32,10 @@ export class Application {
 
   public start(): void {
     this._renderer.gl.clearColor(0, 0, 0, 1)
-    
+
     this._loadShaders()
     this._shader.use()
+    this._createBuffer()
 
     this._loop(0)
   }
@@ -44,6 +47,8 @@ export class Application {
       target.onresize = (event) => {
         canvas.width = event.target.innerWidth
         canvas.height = event.target.innerHeight
+
+        this._renderer.gl.viewport(0, 0, canvas.width, canvas.height)
       }
     }
   }
@@ -54,16 +59,45 @@ export class Application {
     if (canvas !== undefined) {
       canvas.width = window.innerWidth
       canvas.height = window.innerHeight
+
+      this._renderer.gl.viewport(0, 0, canvas.width, canvas.height)
     }
   }
 
   private _loadShaders(): void {
     this._shader = new Shader(this._renderer, "test", vertexShaderSource, fragmentShaderSource)
+  }
 
+  private _createBuffer(): void {
+    const gl = this._renderer.gl
+
+    this._buffer = gl.createBuffer()
+
+    const vertices = [
+      // x, y, z
+      0.0, 0.0, 0.0,
+      0.5, 1, 0.0,
+      0.5, -1, 0.0,
+    ]
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer)
+    // gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0)
+    // gl.enableVertexAttribArray(0)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+    gl.disableVertexAttribArray(0)
   }
 
   private _loop(elapsed: number): void {
-    this._renderer.gl.clear(this._renderer.gl.COLOR_BUFFER_BIT)
+    const gl = this._renderer.gl
+
+    gl.clear(this._renderer.gl.COLOR_BUFFER_BIT)
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer)
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(0)
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
+
 
     requestAnimationFrame(this._loop.bind(this))
   }
